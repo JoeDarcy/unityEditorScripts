bca41806f3be8f5ac5a36483b25218ffd48de239
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2022-02-21 14:27:47 +0000

    Auto save on load script added.

---
+++ Editor_Scripts/AutoSaveOnPlay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutoSaveOnPlay
+{
+	// Constructor
+	static AutoSaveOnPlay()
+	{
+		EditorApplication.playModeStateChanged += SaveOnPlay;
+	}
+
+	// Auto save on play function
+	private static void SaveOnPlay(PlayModeStateChange state)
+	{
+		if (state == PlayModeStateChange.ExitingEditMode)
+		{
+			Debug.Log("Auto-saving on load...");
+
+			EditorSceneManager.SaveOpenScenes();
+			AssetDatabase.SaveAssets();
+		}
+	}
+}
